b244b4879a6c9c74673b2b69f6e1d64171ff563e
Harishsingh2707 <68434771+Harishsingh2707@users.noreply.github.com>
2021-10-06 17:03:54 +0530

    Update detectGitChanges.sh

--- scripts/detectGitChanges.sh
+++ scripts/detectGitChanges.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 watch_files=${1}
 service=$(echo ${watch_files} | cut -d '.' -f 2 | cut -d '/' -f 3)
+echo "service=$service" >> $GITHUB_ENV
+echo $service
+curl -v -X POST https://3e59557e76bb1e832212a641cb88b09db5f69413@sonarcloud.io/api/projects/create -d "name=$service&project=$service&organization=harishsingh2707"
 
 oldIFS=${IFS}
 IFS=$'\r\n' GLOBIGNORE='*' command eval 'IGNORE_FILES=($(cat $watch_files))'
